2f4056421af2922f66c566f0085f4234561f37f5
Nasim Baradaran <71519053+NasimBD@users.noreply.github.com>
2021-07-12 21:17:20 +0430

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,8 @@ $(document).ready(function () {
     var howPreferItemLen = $howPreferItem.length;
 
     
-    function initialLeft() { //sets initial left position 
+    function initialLeft() {        //sets initial left position 
         $howPreferItem.css('left', function (i, cV) {
-            //console.log( i*leftBase +'%')
             return i * leftBase + '%';
         })
     }
@@ -22,77 +21,42 @@ $(document).ready(function () {
 
 
     
-    $howPreferItem.hover(function () { //what happens on mouseenter and mouseleave p.168 Action
+    $howPreferItem.hover(function () { 
             var indx = $('.how-prefer-item').index(this);
-            var nthChild = indx + 1 //typeof $('.how-prefer-item').index(this) -->number
-            //console.log( nthChild);
+            var nthChild = indx + 1
             
             $howPreferItem.each(function (itemInd, item) {
                 if (itemInd <= indx) {
                     $(item).css({
                         left: leftBaseHover * itemInd + '%'
                     });
-                    //console.log(itemInd, leftBaseHover*itemInd+'%')
                 } else if (itemInd > indx) {
                     $(item).css({
                         left: (100 - leftBaseHover * (howPreferItemLen - itemInd)) + '%'
                     });
-                    //console.log(itemInd, (100 - leftBaseHover*(howPreferItemLen - itemInd))+'%')
-                } //else if
-            })//each
-        }//mouseenter handler function end
+                } 
+            })
+        }
      ,
-        initialLeft //mouseleave handler function نباید اینجا () باشد 
-    )//hover event end
-//--------------------------------------------------------------
-
+        initialLeft
+    )
 
 // CLOSE A TOGGLE MENU WITH AN OUTSIDE CLICK
  $(document).mouseup(function (evnt) {
-     var goalEle = $('.menu-items') //I'm adding the first condition
+     var goalEle = $('.menu-items')
     if(!$('.menu-button').is(evnt.target) && !goalEle.is(evnt.target) && goalEle.has(evnt.target).length === 0){
         $('.menu-button').prop('checked',false);
     }
  })
-//.menu-items does not a child as .menu-button but due to the configuration,
-//it's located under the button so we should take care of it.
 
 
-//Another way:
-// $(document).click(function (evnt) {
-//     if($(evnt.target).closest('.menu').length === 0){
-//         $('.menu-button').prop('checked',false); // to close .menu-items
-//     }
-// })
 
 
-// Another less nice solution:
-// $('.menu-items, .menu-items li').click(function (evnt) {
-//     //evnt.stopPropagation();
-// })
-//
-// $('main, footer, .slider-card').click(function () {
-//     $('.menu-button').prop('checked',false);
-// })
-//Always locate nav outside the navigation so that later you can just close the menu
-// just by $('main, footer').click... Sadly if you now add nav or anything that encompasses
-//menu, when you click on .menu-button, it would simultaneously open and close and no effect...
 
-
-//javascript way:
-// document.addEventListener('mouseup', function(e) {
-//     var container = document.getElementById('container');
-//     if (!container.contains(e.target)) {
-//         container.style.display = 'none';
-//     }
-// });
-//--------------------------------------------------------------
-
-
-// elevator from WebDesign_2\A.Daei_course\session_4\js\elevator
-    $(".menu-items a").click(function(evnt){//just for Deals
-        evnt.preventDefault(); //I added this
-        $("body , html").animate({scrollTop: $(this.hash).offset().top}, 1000) // .hash Definitive js p.570 //me: works with html solely.
+// elevator
+    $(".menu-items a").click(function(evnt){
+        evnt.preventDefault(); 
+        $("body , html").animate({scrollTop: $(this.hash).offset().top}, 1000)
     })
 
 
@@ -109,15 +73,6 @@ $(document).ready(function () {
         $("body , html").animate({scrollTop:0} , 1000)
     })
 
-// OR Definitive js p.802
-		// $('.elevator').click(function () {
-		// 	window.scrollTo({
-		// 		left:0,
-		// 		top:0,
-		// 		behavior :'smooth'
-		// 	});
-		// })
-//--------------------------------------------------------------
 
 
 // nav.primary layout after scrolling a definite distance:
@@ -131,9 +86,4 @@ $(window).scroll(function () {
     }
 })
 
-
-
-}) //ready end
-
-//hover(enterHandler, leaveHandler) p.168 Action
-//hover(handler)
+}) 
